0a0a8a546cbee010181a3b31b04f14b7d353c194
Elandiel <ma.hambach@gmx.net>
2024-01-23 16:40:27 +0100

    meaningfull change: main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String [] args){
         // Comments are for people without perfect memory.
         String name = "Hello World!" ;
-        System.out.println(name);
+        System.out.println(name+name);
     }
 
     public static void usefulMethod(){
