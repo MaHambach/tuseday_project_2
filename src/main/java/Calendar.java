41fa5594bf4acd84f6c2b1dbe2c4fbbcaf9e4273
Elandiel <ma.hambach@gmx.net>
2024-01-23 17:41:48 +0100

    new function: printNumbers

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -17,6 +17,15 @@ public class Calendar {
 
     public static void printCalendarForMonth(String monthName){
         System.out.println(monthName);
+        System.out.println("Mo Tu We Th Fr Sa Su");
+        printNumbers(31);
         // weitere funktionen hier einfügen?
+    }
+
+    public static void printNumbers(int numberOfDays){
+        for(int i = 1; i<numberOfDays+1; i++){
+            System.out.format("%2d ", i);
+            if(i % 7==0 ) System.out.println();
+        }
     }
 }
